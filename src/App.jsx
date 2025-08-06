bd3da54478ae1959e8899c5e5d7b84ed55d92f3c
KaberaSamuel <kaberanshutis@gmail.com>
2025-08-06 02:23:04 +0200

    basic redux toolkit setup in react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
